7efaf4b6d861c75d824cf3c04b27c36956f37701
Bayron Ortiz <bayrondanilo92@gmail.com>
2022-07-17 18:30:26 -0500

    Agregado acciones personalizadas eliminación de productos listado de compras

--- shopping_list/api/viewsets.py
+++ shopping_list/api/viewsets.py
@@ -3,7 +3,9 @@
 # Django imports
 
 # DRF imports
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Models imports
 from shopping_list.models import ShoppingItem
@@ -15,3 +17,17 @@ from shopping_list.api.serializers import ShoppingItemSerializer
 class ShoppingItemViewSet(viewsets.ModelViewSet):
     queryset = ShoppingItem.objects.all()
     serializer_class = ShoppingItemSerializer
+
+    @action(detail=False, methods=['DELETE'], url_path='delete-all-purchased', url_name='delete-all-purchased')
+    def delete_purchased(self, request):
+        """Custom action to bulk delete data"""
+        ShoppingItem.objects.filter(purchased=True).delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    @action(detail=False, methods=['DELETE'], url_path='delete-all', url_name='delete-all')
+    def delete_all(self, request):
+        """Custom action to bulk delete data"""
+        ShoppingItem.objects.all().delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
