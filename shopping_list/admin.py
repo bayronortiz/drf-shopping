53746c618fc05d298c49727ac89b78735711f8a7
Bayron Ortiz <bayrondanilo92@gmail.com>
2022-07-23 15:51:35 -0500

    Agregado nuevo modelo ShoppingList para gestión de múltiples listas de productos

--- shopping_list/admin.py
+++ shopping_list/admin.py
@@ -2,7 +2,8 @@
 from django.contrib import admin
 
 # Models imports
-from shopping_list.models import ShoppingItem
+from shopping_list.models import ShoppingItem, ShoppingList
 
 
+admin.site.register(ShoppingList)
 admin.site.register(ShoppingItem)
--- shopping_list/models.py
+++ shopping_list/models.py
@@ -5,10 +5,19 @@ import uuid
 from django.db import models
 
 
+class ShoppingList(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class ShoppingItem(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     purchased = models.BooleanField(default=False)
+    shopping_list = models.ForeignKey(ShoppingList, on_delete=models.CASCADE, related_name='shopping_items')
 
     def __str__(self):
         return self.name
